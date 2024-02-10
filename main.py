5b2bce61d0c655edde35d57ef4ac8cc38f35cf65
Zija Igidov <igidov.zija@gmail.com>
2024-02-10 21:29:58 +0100

    Add Player class to represent a player
    Added a Player class to represent a player in the game of chess. Right
    now, it only contains a single attribute `color`, but as more features
    are added to the game, additional attributes such as `moves_played` and
    `move_history` may be added.

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+
+class Player:
+    """A player."""
+
+    def __init__(self, color):
+        """Initializes the player object with a color."""
+        self.color = color
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
