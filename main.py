7ee27c1488bd177c02217adcbf8920b7760f9416
Zija Igidov <igidov.zija@gmail.com>
2024-02-11 10:20:32 +0100

    Add Gameboard class to represent the game board
    Added a Gameboard class that contains the state of the game board, with
    the __init__() dunder method to set the initial board.

--- main.py
+++ main.py
@@ -8,9 +8,34 @@ class Player:
         self.color = color
 
 
+class Gameboard:
+    """The chess game board."""
+
+    '''
+    The board is implemented as a 2D list, where the first index relates to the
+    rank and the second index relates to the number of the file.
+    - board[0][0] corresponds to the top-left square
+    - board[7][7] corresponds to the bottom-right square
+    '''
+    INITIAL_BOARD = [
+        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+    ]
+
+    def __init__(self):
+        """Initialize the game board."""
+        self.__board = Gameboard.INITIAL_BOARD
+
+
 def main():
     pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
