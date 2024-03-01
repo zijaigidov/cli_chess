5a1af1ffdd17b60a868901cfcda773f83d121892
Zija Igidov <igidov.zija@gmail.com>
2024-03-01 18:12:30 +0100

    Add unit test for is_valid_square()
    Add a unit test for the is_valid_square() method of the Square class.

---
+++ tests/unit_test.py
@@ -0,0 +1,9 @@
+# unit_test.py
+
+from ..cli_chess.square import Square
+
+
+def test_is_valid_square():
+    assert Square.is_valid_square('e4') == True
+    assert Square.is_valid_square('a9') == False
+    assert Square.is_valid_square('k2') == False
