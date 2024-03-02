5342882765c04b502c2af2b40864038f82d225e1
Zija Igidov <igidov.zija@gmail.com>
2024-03-02 12:14:53 +0100

    Add tests for MoveValidator's distance helper functions
    Add unit tests for MoveValidator's distance helper functions
    _get_file_distance() and _get_rank_distance().

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,15 +1,11 @@
 # unit_test.py
 
-from ..cli_chess.square import Square
 from ..cli_chess.gameboard import Gameboard
+from ..cli_chess.move_validator import MoveValidator
+from ..cli_chess.square import Square
 
 
-def test_is_valid_square():
-    assert Square.is_valid_square('e4') == True
-    assert Square.is_valid_square('a9') == False
-    assert Square.is_valid_square('k2') == False
-
-
+# gameboard.py
 def test_is_valid_initial_board():
     VALID_INITIAL_BOARD = [
         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
@@ -29,3 +25,21 @@ def test_is_valid_initial_board():
     for row in range(-2, 0):
         for col in range(8):
             assert board[row][col].symbol == VALID_INITIAL_BOARD[row][col]
+
+
+# move_validator.py
+def test_get_file_distance():
+    assert MoveValidator._get_file_distance('a', 'h') == 7
+    assert MoveValidator._get_file_distance('h', 'a') == 7
+
+
+def test_get_rank_distance():
+    assert MoveValidator._get_rank_distance('1', '8') == 7
+    assert MoveValidator._get_rank_distance('8', '1') == 7
+
+
+# square.py
+def test_is_valid_square():
+    assert Square.is_valid_square('e4') == True
+    assert Square.is_valid_square('a9') == False
+    assert Square.is_valid_square('k2') == False
